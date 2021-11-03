a808e1eb7f747251560189d36f87fdcf6e2e524f
Manuel Guidon <guidon@itis.swiss>
2021-11-03 14:35:23 +0100

    minor cleanup

--- volume-sync/scripts/startup.py
+++ volume-sync/scripts/startup.py
@@ -1,49 +1,66 @@
 #!/usr/bin/env python3
 
-import socket
-import os
-import struct
 import logging
-import subprocess
+import os
 import shlex
 import shutil
-import time
+import socket
+import struct
+import subprocess
 import sys
+import time
 
 # Application constants
-DEV_NULL = open(os.devnull, 'w')
+DEV_NULL = open(os.devnull, "w")
 SYNC_TYPES = ["NEXT", "FIRST", "ALL"]
 SYNC_SERVER_PORT = 2222
-SYNC_SERVER = ["unison","-socket", str(SYNC_SERVER_PORT)]
-SYNC_CLIENT = ["unison", "-auto", "-batch","-fastcheck",\
-               "-group","-owner","-prefer=newer","-silent",\
-               "-times","-confirmbigdel=false", "-confirmmerge=false"]
+SYNC_SERVER = ["unison", "-socket", str(SYNC_SERVER_PORT)]
+SYNC_CLIENT = [
+    "unison",
+    "-auto",
+    "-batch",
+    "-fastcheck",
+    "-group",
+    "-owner",
+    "-prefer=newer",
+    "-silent",
+    "-times",
+    "-confirmbigdel=false",
+    "-confirmmerge=false",
+]
 # Pre setting
-if os.getenv('DEBUG'):
+if os.getenv("DEBUG"):
     logging.basicConfig(level=logging.DEBUG)
     logging.debug("Debugging log level is enabled")
 else:
     logging.basicConfig(level=logging.INFO)
 
 # Application environments
-SYNC_INTERVAL = int(os.getenv('SYNC_INTERVAL', "0")) # MAX interval in seconds
-SYNC_TIMEOUT = int(os.getenv('SYNC_TIMEOUT', -1))
+SYNC_INTERVAL = int(os.getenv("SYNC_INTERVAL", "0"))  # MAX interval in seconds
+SYNC_TIMEOUT = int(os.getenv("SYNC_TIMEOUT", -1))
 if SYNC_TIMEOUT < 0:
     SYNC_TIMEOUT = None
-HOSTNAME_GROUP = os.getenv('HOSTNAME', 'sync')
-SYNC_TYPE = os.getenv('SYNC_TYPE', SYNC_TYPES[0]).upper() # Sync type for distributed sync
+HOSTNAME_GROUP = os.getenv("HOSTNAME", "sync")
+SYNC_TYPE = os.getenv(
+    "SYNC_TYPE", SYNC_TYPES[0]
+).upper()  # Sync type for distributed sync
 if not SYNC_TYPE in SYNC_TYPES:
-    logging.info("%s is no valid sync type. Set to default %s" % (SYNC_TYPE, SYNC_TYPE[0]))
+    logging.info(
+        "%s is no valid sync type. Set to default %s" % (SYNC_TYPE, SYNC_TYPE[0])
+    )
     SYNC_TYPE = SYNC_TYPES[0]
-WAIT_BEFORE_SYNC = int(os.getenv('WAIT_BEFORE_SYNC', "10")) # Wait before servers are ready
-SYNC_FOLDER = os.getenv('SYNC_FOLDER', "/volumes")
-ADDITIONAL_OPTIONS = shlex.split(os.getenv('ADDITIONAL_OPTIONS', ""))
-if os.getenv('ONLY_ADDITIONAL_OPTIONS'):
+WAIT_BEFORE_SYNC = int(
+    os.getenv("WAIT_BEFORE_SYNC", "10")
+)  # Wait before servers are ready
+SYNC_FOLDER = os.getenv("SYNC_FOLDER", "/volumes")
+ADDITIONAL_OPTIONS = shlex.split(os.getenv("ADDITIONAL_OPTIONS", ""))
+if os.getenv("ONLY_ADDITIONAL_OPTIONS"):
     SYNC_CLIENT = ["unison"]
 
 # Vars
 shutdown = False
 server_process = None
+
 
 def get_group_ips():
     # Get the ips of all replicas without that of this container
@@ -53,13 +70,18 @@ def get_group_ips():
         sync_group_ips = []
     return sync_group_ips
 
+
 def get_sorted_group_ips():
     group_ips = get_group_ips()
     # Sort ips from 0.0.0.0 to 255.255.255.255 by each byte value
-    return sorted(group_ips, key=lambda ip: struct.unpack("!L", socket.inet_aton(ip))[0])
+    return sorted(
+        group_ips, key=lambda ip: struct.unpack("!L", socket.inet_aton(ip))[0]
+    )
+
 
 def get_container_ip():
-    return socket.gethostbyname_ex(socket.gethostname()+".")[2][0]
+    return socket.gethostbyname_ex(socket.gethostname() + ".")[2][0]
+
 
 def check_sync_server():
     # Shutdown if sync server has stopped
@@ -69,14 +91,17 @@ def check_sync_server():
         logging.warn("Sync server is not running anymore. Shutting down!")
         SHUTDOWN = True
 
+
 def cleanup():
-    shutil.rmtree('/root/.unison', ignore_errors=True)
+    shutil.rmtree("/root/.unison", ignore_errors=True)
+
 
 def start_sync_server():
     global server_process
     args = SYNC_SERVER
     server_process = subprocess.Popen(args)
     logging.debug("Sync server started with args: %s" % args)
+
 
 def try_kill_process(process):
     try:
@@ -92,11 +117,16 @@ def sync():
     container_ip = get_container_ip()
     logging.debug("Container ip is %s" % container_ip)
     if container_ip not in all_sync_ips:
-        logging.error("IP of Container %s not part of group %s. Do you have all connected the hosts via network?" % (container_ip, all_sync_ips))
+        logging.error(
+            "IP of Container %s not part of group %s. Do you have all connected the hosts via network?"
+            % (container_ip, all_sync_ips)
+        )
     container_ip_pos = all_sync_ips.index(container_ip)
     # This container must be part of the network
     if container_ip_pos < 0:
-        logging.warn("Container ip %s not in group ip %s" % (container_ip, all_sync_ips))
+        logging.warn(
+            "Container ip %s not in group ip %s" % (container_ip, all_sync_ips)
+        )
     # Create list of sync targets
     sync_ips = []
     if SYNC_TYPE == "FIRST":
@@ -126,11 +156,12 @@ def sync():
         args = SYNC_CLIENT + ADDITIONAL_OPTIONS + [sync_source] + [sync_target]
         logging.info("Running sync (Timeout: %s) with args: %s " % (SYNC_TIMEOUT, args))
         sync_process = subprocess.Popen(args)
-        returncode = sync_process.wait(timeout = SYNC_TIMEOUT)
-        if (returncode == None):
+        returncode = sync_process.wait(timeout=SYNC_TIMEOUT)
+        if returncode == None:
             logging.warn("Could not finish sync in timeout %s" % SYNC_TIMEOUT)
             try_kill_process(sync_process)
         logging.debug("Sync process stopped with exit code %s" % returncode)
+
 
 ## MAIN
 cleanup()
@@ -140,11 +171,11 @@ time.sleep(WAIT_BEFORE_SYNC)
 lastSync = time.time()
 while not shutdown:
     check_sync_server()
-    if (time.time()-lastSync > SYNC_INTERVAL):
+    if time.time() - lastSync > SYNC_INTERVAL:
         logging.debug("Now syncing")
         lastSync = time.time()
         sync()
-    nextSync = SYNC_INTERVAL - (time.time()-lastSync)
+    nextSync = SYNC_INTERVAL - (time.time() - lastSync)
     if nextSync > 0:
         logging.debug("Next sync in %ss" % nextSync)
         time.sleep(nextSync)
