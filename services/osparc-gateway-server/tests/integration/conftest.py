fdb75251263c909bbfa3dad4e7ba61a9760dc18b
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-12-13 16:56:02 +0100

    integration test for testing computational sidecar

---
+++ services/osparc-gateway-server/tests/integration/conftest.py
@@ -0,0 +1,147 @@
+import asyncio
+from pathlib import Path
+from typing import Any, AsyncIterator, Awaitable, Callable, Dict
+
+import aiodocker
+import dask_gateway
+import pytest
+import traitlets
+import traitlets.config
+from _dask_helpers import DaskGatewayServer
+from _pytest.tmpdir import tmp_path
+from dask_gateway_server.app import DaskGateway
+from faker import Faker
+from osparc_gateway_server.backend.osparc import (
+    OsparcClusterConfig,
+    UnsafeOsparcBackend,
+)
+from tenacity._asyncio import AsyncRetrying
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_fixed
+
+
+@pytest.fixture
+async def docker_network(
+    async_docker_client: aiodocker.Docker,
+) -> AsyncIterator[Callable[[str], Awaitable[Dict[str, Any]]]]:
+    networks = []
+
+    async def _network_creator(name: str) -> Dict[str, Any]:
+        network = await async_docker_client.networks.create(
+            config={"Name": name, "Driver": "overlay"}
+        )
+        assert network
+        networks.append(network)
+        return await network.show()
+
+    yield _network_creator
+
+    # wait until all networks are really gone
+    async def _wait_for_network_deletion(network: aiodocker.docker.DockerNetwork):
+        network_name = (await network.show())["Name"]
+        await network.delete()
+        async for attempt in AsyncRetrying(
+            reraise=True, wait=wait_fixed(1), stop=stop_after_delay(60)
+        ):
+            with attempt:
+                print(f"<-- waiting for network '{network_name}' deletion...")
+                list_of_network_names = [
+                    n["Name"] for n in await async_docker_client.networks.list()
+                ]
+                assert network_name not in list_of_network_names
+            print(f"<-- network '{network_name}' deleted")
+
+    await asyncio.gather(*[_wait_for_network_deletion(network) for network in networks])
+
+
+@pytest.fixture
+async def docker_volume(
+    async_docker_client: aiodocker.Docker,
+) -> AsyncIterator[Callable[[str], Awaitable[Dict[str, Any]]]]:
+    volumes = []
+
+    async def _volume_creator(name: str) -> Dict[str, Any]:
+        volume = await async_docker_client.volumes.create(config={"Name": name})
+        assert volume
+        volumes.append(volume)
+        return await volume.show()
+
+    yield _volume_creator
+    # cleanup
+    async def _wait_for_volume_deletion(volume: aiodocker.docker.DockerVolume):
+        inspected_volume = await volume.show()
+        async for attempt in AsyncRetrying(reraise=True, wait=wait_fixed(1)):
+            with attempt:
+                print(f"<-- deleting volume '{inspected_volume['Name']}'...")
+                await volume.delete()
+            print(f"<-- volume '{inspected_volume['Name']}' deleted")
+
+    await asyncio.gather(*[_wait_for_volume_deletion(v) for v in volumes])
+
+
+@pytest.fixture
+def gateway_password(faker: Faker) -> str:
+    return faker.password()
+
+
+@pytest.fixture()
+def cluster_directory(tmp_path, faker: Faker) -> Path:
+    dir = Path(tmp_path / faker.pystr())
+    dir.mkdir(parents=True, exist_ok=True)
+    assert dir.exists()
+    return dir
+
+
+@pytest.fixture
+async def local_dask_gateway_server(
+    minimal_config: None, gateway_password: str, cluster_directory: Path
+) -> AsyncIterator[DaskGatewayServer]:
+    """this code is more or less copy/pasted from dask-gateway repo"""
+    c = traitlets.config.Config()
+    c.DaskGateway.backend_class = UnsafeOsparcBackend  # type: ignore
+    c.DaskGateway.Backend.cluster_config_class = OsparcClusterConfig  # type: ignore
+    c.DaskGateway.address = "127.0.0.1:0"  # type: ignore
+    c.DaskGateway.log_level = "DEBUG"  # type: ignore
+    c.Proxy.address = "127.0.0.1:0"  # type: ignore
+    c.DaskGateway.authenticator_class = "dask_gateway_server.auth.SimpleAuthenticator"  # type: ignore
+    c.SimpleAuthenticator.password = gateway_password  # type: ignore
+    c.OsparcBackend.clusters_directory = f"{cluster_directory}"  # type: ignore
+    print(f"--> local dask gateway config: {c}")
+    dask_gateway_server = DaskGateway(config=c)
+    dask_gateway_server.initialize([])  # that is a shitty one!
+    print("--> local dask gateway server initialized")
+    await dask_gateway_server.setup()
+    await dask_gateway_server.backend.proxy._proxy_contacted  # pylint: disable=protected-access
+    print("--> local dask gateway server setup completed")
+    yield DaskGatewayServer(
+        f"http://{dask_gateway_server.backend.proxy.address}",
+        f"gateway://{dask_gateway_server.backend.proxy.tcp_address}",
+        c.SimpleAuthenticator.password,  # type: ignore
+        dask_gateway_server,
+    )
+    print("--> local dask gateway server switching off...")
+    await dask_gateway_server.cleanup()
+    print("...done")
+
+
+@pytest.fixture
+async def gateway_client(
+    local_dask_gateway_server: DaskGatewayServer,
+) -> AsyncIterator[dask_gateway.Gateway]:
+    async with dask_gateway.Gateway(
+        local_dask_gateway_server.address,
+        local_dask_gateway_server.proxy_address,
+        asynchronous=True,
+        auth=dask_gateway.BasicAuth(
+            username="pytest_user", password=local_dask_gateway_server.password
+        ),
+    ) as gateway:
+        assert gateway
+        print(f"--> {gateway} created")
+        cluster_options = await gateway.cluster_options()
+        gateway_versions = await gateway.get_versions()
+        clusters_list = await gateway.list_clusters()
+        print(f"--> {gateway_versions}, {cluster_options}, {clusters_list}")
+        for option in cluster_options.items():
+            print(f"--> {option}")
+        yield gateway
--- services/osparc-gateway-server/tests/integration/test_integration.py
+++ services/osparc-gateway-server/tests/integration/test_integration.py
@@ -3,22 +3,16 @@
 
 import asyncio
 import socket
-from os import name
-from typing import AsyncIterator, NamedTuple
+from typing import Any, Awaitable, Callable, Dict
 
-import aiodocker
 import pytest
-import traitlets.config
+from _dask_helpers import DaskGatewayServer
+from _host_helpers import get_this_computer_ip
+from _pytest.monkeypatch import MonkeyPatch
 from aiodocker import Docker
-from dask_gateway import BasicAuth, Gateway
-from dask_gateway_server.app import DaskGateway
-from osparc_gateway_server.backend.osparc import (
-    OsparcClusterConfig,
-    UnsafeOsparcBackend,
-)
-from pytest_mock.plugin import MockerFixture
+from dask_gateway import Gateway
+from faker import Faker
 from tenacity import retry
-from tenacity._asyncio import AsyncRetrying
 from tenacity.stop import stop_after_attempt
 from tenacity.wait import wait_fixed
 
@@ -27,143 +21,20 @@ TMP_FOLDER = "/tmp/gateway"
 
 
 @pytest.fixture
-async def gateway_workers_network(
-    async_docker_client: aiodocker.Docker,
-) -> AsyncIterator[str]:
-    _NETWORK_NAME = "pytest_swarm_network"
-    network = await async_docker_client.networks.create(
-        config={"Name": _NETWORK_NAME, "Driver": "overlay"}
-    )
-    assert network
-
-    yield _NETWORK_NAME
-    await network.delete()
-    async for attempt in AsyncRetrying(reraise=True, wait=wait_fixed(1)):
-        with attempt:
-            print(f"<-- waiting for network '{_NETWORK_NAME}' deletion...")
-            list_of_network_names = [
-                network["Name"] for network in await async_docker_client.networks.list()
-            ]
-            assert _NETWORK_NAME not in list_of_network_names
-        print(f"<-- network '{_NETWORK_NAME}' deleted")
-
-
-@pytest.fixture
-async def gateway_volume_name(
-    async_docker_client: aiodocker.Docker,
-) -> AsyncIterator[str]:
-    _VOLUME_NAME = "pytest_gateway_volume"
-    volume = await async_docker_client.volumes.create(config={"Name": _VOLUME_NAME})
-    assert volume
-    yield _VOLUME_NAME
-    async for attempt in AsyncRetrying(reraise=True, wait=wait_fixed(1)):
-        with attempt:
-            print(f"<-- deleting volume '{_VOLUME_NAME}'...")
-            await volume.delete()
-        print(f"<-- volume '{_VOLUME_NAME}' deleted")
-
-@pytest.fixture
-async def computational_volume_name(
-    async_docker_client: aiodocker.Docker,
-) -> AsyncIterator[str]:
-    _VOLUME_NAME = "pytest_computational_volume"
-    volume = await async_docker_client.volumes.create(config={"Name": _VOLUME_NAME})
-    assert volume
-    yield _VOLUME_NAME
-    async for attempt in AsyncRetrying(reraise=True, wait=wait_fixed(1)):
-        with attempt:
-            print(f"<-- deleting volume '{_VOLUME_NAME}'...")
-            await volume.delete()
-        print(f"<-- volume '{_VOLUME_NAME}' deleted")
-
-def get_ip() -> str:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(("10.255.255.255", 1))
-        IP = s.getsockname()[0]
-    except Exception:  # pylint: disable=broad-except
-        IP = "127.0.0.1"
-    finally:
-        s.close()
-    return IP
-
-
-@pytest.fixture
 def minimal_config(
-    docker_swarm, monkeypatch, gateway_workers_network: str, gateway_volume_name: str, computational_volume_name: str
+    loop: asyncio.AbstractEventLoop,
+    monkeypatch: MonkeyPatch,
+    faker: Faker,
 ):
-    monkeypatch.setenv("GATEWAY_VOLUME_NAME", gateway_volume_name)
+    monkeypatch.setenv("GATEWAY_VOLUME_NAME", faker.pystr())
     monkeypatch.setenv("GATEWAY_WORK_FOLDER", "/tmp/pytest_work_folder")
-    monkeypatch.setenv("GATEWAY_WORKERS_NETWORK", gateway_workers_network)
-    monkeypatch.setenv("GATEWAY_SERVER_NAME", get_ip())
-    monkeypatch.setenv("COMPUTATIONAL_SIDECAR_VOLUME_NAME", computational_volume_name)
+    monkeypatch.setenv("GATEWAY_WORKERS_NETWORK", faker.pystr())
+    monkeypatch.setenv("GATEWAY_SERVER_NAME", get_this_computer_ip())
+    monkeypatch.setenv("COMPUTATIONAL_SIDECAR_VOLUME_NAME", faker.pystr())
     monkeypatch.setenv(
         "COMPUTATIONAL_SIDECAR_IMAGE",
         "itisfoundation/dask-sidecar:master-github-latest",
     )
-    
-
-
-class DaskGatewayServer(NamedTuple):
-    address: str
-    proxy_address: str
-    password: str
-    server: DaskGateway
-
-
-@pytest.fixture
-async def local_dask_gateway_server(
-    loop: asyncio.AbstractEventLoop,
-    minimal_config: None,
-) -> AsyncIterator[DaskGatewayServer]:
-    """this code is more or less copy/pasted from dask-gateway repo"""
-    c = traitlets.config.Config()
-    c.DaskGateway.backend_class = UnsafeOsparcBackend  # type: ignore
-    c.DaskGateway.Backend.cluster_config_class = OsparcClusterConfig  # type: ignore
-    c.DaskGateway.address = "127.0.0.1:0"  # type: ignore
-    c.DaskGateway.log_level = "DEBUG"  # type: ignore
-    c.Proxy.address = "127.0.0.1:0"  # type: ignore
-    c.DaskGateway.authenticator_class = "dask_gateway_server.auth.SimpleAuthenticator"  # type: ignore
-    c.SimpleAuthenticator.password = PASSWORD  # type: ignore
-    c.OsparcBackend.clusters_directory = TMP_FOLDER  # type: ignore
-    print(f"--> local dask gateway config: {c}")
-    dask_gateway_server = DaskGateway(config=c)
-    dask_gateway_server.initialize([])  # that is a shitty one!
-    print("--> local dask gateway server initialized")
-    await dask_gateway_server.setup()
-    await dask_gateway_server.backend.proxy._proxy_contacted  # pylint: disable=protected-access
-    print("--> local dask gateway server setup completed")
-    yield DaskGatewayServer(
-        f"http://{dask_gateway_server.backend.proxy.address}",
-        f"gateway://{dask_gateway_server.backend.proxy.tcp_address}",
-        c.SimpleAuthenticator.password,  # type: ignore
-        dask_gateway_server,
-    )
-    print("--> local dask gateway server switching off...")
-    await dask_gateway_server.cleanup()
-    print("...done")
-
-
-@pytest.fixture
-async def gateway_client(
-    local_dask_gateway_server: DaskGatewayServer,
-) -> AsyncIterator[Gateway]:
-    async with Gateway(
-        local_dask_gateway_server.address,
-        local_dask_gateway_server.proxy_address,
-        asynchronous=True,
-        auth=BasicAuth(username="pytest_user", password=PASSWORD),
-    ) as gateway:
-        assert gateway
-        print(f"--> {gateway} created")
-        cluster_options = await gateway.cluster_options()
-        gateway_versions = await gateway.get_versions()
-        clusters_list = await gateway.list_clusters()
-        print(f"--> {gateway_versions}, {cluster_options}, {clusters_list}")
-        for option in cluster_options.items():
-            print(f"--> {option}")
-        yield gateway
 
 
 @retry(reraise=True, stop=stop_after_attempt(20), wait=wait_fixed(0.1))
@@ -195,12 +66,26 @@ async def test_cluster_start_stop(minimal_config, gateway_client: Gateway):
         clusters = await gateway_client.list_clusters()
         assert clusters == []
 
-@pytest.mark.skip("not ready")
-async def test_cluster_scale(docker_swarm, minimal_config, gateway_client: Gateway, mocker: MockerFixture):
-    mocked_service_create_func = mocker.patch("aiodocker.services.DockerServices.create", return_value={"ID": "pytest_mock_id"})
-    mocked_service_inspec_func = mocker.patch("aiodocker.services.DockerServices.inspect", return_value={"ID": "pytest_mock_id", "Spec": {"Name": "pytest_fake_service"}})
-    mocked_is_task_running_func = mocker.patch("osparc_gateway_server.backend.osparc._is_task_running", return_value=True)
-    mocked_service_delete_func = mocker.patch("aiodocker.services.DockerServices.delete", return_value=None)
+
+@pytest.fixture
+async def gateway_worker_network(
+    local_dask_gateway_server: DaskGatewayServer,
+    docker_network: Callable[[str], Awaitable[Dict[str, Any]]],
+) -> Dict[str, Any]:
+    network = await docker_network(
+        local_dask_gateway_server.server.backend.settings.GATEWAY_WORKERS_NETWORK
+    )
+    return network
+
+
+@pytest.mark.skip(reason="not ready yet")
+async def test_cluster_scale(
+    docker_swarm: None,
+    docker_network,
+    minimal_config: None,
+    gateway_client: Gateway,
+    gateway_worker_network: Dict[str, Any],
+):
 
     # No currently running clusters
     clusters = await gateway_client.list_clusters()
@@ -218,7 +103,7 @@ async def test_cluster_scale(docker_swarm, minimal_config, gateway_client: Gatew
 
         # we should have 2 services
         await asyncio.sleep(5)
-        mocked_service_create_func.assert_called()
+        # mocked_service_create_func.assert_called()
         # await wait_for_n_services(2)
 
         # and 2 corresponding containers
@@ -252,6 +137,7 @@ async def test_cluster_scale(docker_swarm, minimal_config, gateway_client: Gatew
 
         # and no corresponding container
         await wait_for_n_containers(0)
+
 
 @pytest.mark.skip("not ready")
 async def test_multiple_clusters(minimal_config, gateway_client: Gateway):
