71d0b6872cdc5ffee002a516fbac2200d5abae87
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-12-13 11:04:24 +0100

    add test for checking settings

---
+++ services/osparc-gateway-server/tests/unit/test_settings.py
@@ -0,0 +1,20 @@
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+from osparc_gateway_server.backend.settings import AppSettings
+
+
+@pytest.fixture
+def minimal_config(monkeypatch: MonkeyPatch):
+    monkeypatch.setenv("GATEWAY_VOLUME_NAME", "atestvolumename")
+    monkeypatch.setenv("GATEWAY_WORK_FOLDER", "atestworkfolder")
+    monkeypatch.setenv("GATEWAY_WORKERS_NETWORK", "atestnetwork")
+    monkeypatch.setenv("GATEWAY_SERVER_NAME", "atestserver")
+    monkeypatch.setenv("COMPUTATIONAL_SIDECAR_IMAGE", "test/localpytest:latest")
+    monkeypatch.setenv(
+        "COMPUTATIONAL_SIDECAR_VOLUME_NAME", "sidecar_computational_volume_name"
+    )
+
+
+def test_app_settings(minimal_config):
+    settings = AppSettings()
+    assert settings
